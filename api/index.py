73d2c30e3f32daefd8b3f55f1d75b123af97c01a
nba001 <nba001@eyepaste.com>
2022-12-06 10:29:34 -0800

    Adding site files

--- api/index.py
+++ api/index.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/products')
+def products():
+    return render_template('Products.html')
 
 @app.route('/about')
 def about():
-    return 'About'
+    return render_template('About Us.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('Contact Us.html')
+
+@app.route('/contactForm')
+def contactForm():
+    return render_template('index.html')
+
+
+@app.route('/test')
+def test():
+    return 'Test'
+
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('result.html', result = dict)
