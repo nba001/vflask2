2dc550b96e177864646d731037b1aa77b2ea207d
nba001 <nba001@eyepaste.com>
2022-12-14 10:29:31 -0800

    Updated

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import productsList
 
 app = Flask(__name__)
 
@@ -7,17 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/products')
 def products():
-    return render_template('Products.html')
+    return render_template('Products.html', products=productsList.products)
+
 
 @app.route('/about')
 def about():
     return render_template('About Us.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('Contact Us.html')
+
 
 @app.route('/contactForm')
 def contactForm():
@@ -28,7 +33,8 @@ def contactForm():
 def test():
     return 'Test'
 
+
 @app.route('/result')
 def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+    dict = {'phy': 50, 'che': 60, 'maths': 70}
+    return render_template('result.html', result=dict)
